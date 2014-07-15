8f70006bbe1fd0eec24635acba1f7218dd85d260
taesiri <taesiri@live.com>
2014-07-15 10:42:32 +0430

    [WIP] Automatic Scene Builder

--- ArenaBuilder/Assets/Scripts/Editor/AutoLevelBuilder.cs
+++ ArenaBuilder/Assets/Scripts/Editor/AutoLevelBuilder.cs
@@ -1,32 +1,95 @@
-﻿using UnityEditor;
+﻿using Assets.Scripts.Arena;
+using UnityEditor;
 using UnityEngine;
 
 namespace Assets.Scripts.Editor
 {
     public class AutoLevelBuilder : EditorWindow
     {
-        private bool myBool = true;
-        private float myFloat = 1.23f;
-        private string myString = "Hello World";
-        private bool groupEnabled;
+        private float _camDistance;
+        private bool _isometricSetup;
+        private string _levelName = "new-level";
 
         [MenuItem("DeadMage/Create New Level", false, 100)]
         public static void ShowWindow()
         {
-            var window = GetWindow(typeof(AutoLevelBuilder));
+            EditorWindow window = GetWindow(typeof (AutoLevelBuilder));
             window.title = "New Arena";
         }
 
-
         private void OnGUI()
         {
-            GUILayout.Label("Base Settings", EditorStyles.boldLabel);
-            myString = EditorGUILayout.TextField("Text Field", myString);
+            GUILayout.Label("General Settings", EditorStyles.boldLabel);
+            _levelName = EditorGUILayout.TextField("Level Name", _levelName);
 
-            groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
-            myBool = EditorGUILayout.Toggle("Toggle", myBool);
-            myFloat = EditorGUILayout.Slider("Slider", myFloat, -3, 3);
+
+            _isometricSetup = EditorGUILayout.BeginToggleGroup("Isometric Setup", _isometricSetup);
+            _camDistance = EditorGUILayout.FloatField("Camera Distance", _camDistance);
             EditorGUILayout.EndToggleGroup();
+
+            if (GUILayout.Button("Create !"))
+            {
+                CreateLevel(_isometricSetup);
+            }
+        }
+
+
+        private void CreateLevel(bool isIsometric)
+        {
+            var sceneRoot = new GameObject {name = "SceneObjects"};
+
+
+            //Background
+            GameObject bg = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            bg.transform.localScale = new Vector3(10, 1, 10);
+            bg.transform.position = new Vector3(0, 0, 10);
+            bg.transform.Rotate(Vector3.right, 270);
+            bg.name = "Background";
+            bg.renderer.material = Resources.Load("Materials/DefaultBackgroundMaterial", typeof (Material)) as Material;
+
+
+            //Camera
+            Camera.main.isOrthoGraphic = true;
+            Camera.main.farClipPlane = 100;
+            Camera.main.backgroundColor = Color.black;
+
+            if (!isIsometric)
+            {
+                Camera.main.orthographicSize = 8.6f;
+                Camera.main.transform.parent = sceneRoot.transform;
+            }
+            else
+            {
+                var camContainer = new GameObject("CameraContainer");
+
+                Camera.main.isOrthoGraphic = true;
+                Camera.main.orthographicSize = 5.6f;
+                Camera.main.transform.Rotate(45, 35, 0);
+                Camera.main.transform.parent = camContainer.transform;
+                Camera.main.transform.position = new Vector3(6, 15, -9);
+
+                camContainer.transform.position = new Vector3(-16, 0.4f, 2.1f);
+                camContainer.transform.Rotate(Vector3.right, 270);
+            }
+
+            // Light
+            var dLight = new GameObject();
+            var lightComponent = dLight.AddComponent<Light>();
+            lightComponent.type = LightType.Directional;
+            lightComponent.intensity = 0.5f;
+            lightComponent.cookieSize = 10f;
+            dLight.transform.position = new Vector3(0, 0, -20);
+            dLight.name = "Scene Light";
+            dLight.transform.parent = sceneRoot.transform;
+
+
+            var gridObject = new GameObject("Grid");
+            var grid = gridObject.AddComponent<AdvanceGrid>();
+
+            var brainObject = new GameObject("Brain");
+            var brain = brainObject.AddComponent<AdvanceBrain>();
+
+            brain.GameGrid = grid;
         }
     }
 }
