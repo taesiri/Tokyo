68196ace98a73ec9291a6f5fa58831bbb5a46a96
taesiri <taesiri@live.com>
2014-07-17 16:51:44 +0430

    BreakableTile Added

---
+++ ArenaBuilder/Assets/Scripts/Arena/Tiles/BreakableTile.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Arena.Tiles
+{
+    public class BreakableTile : Deployable
+    {
+        private float _health = 100f;
+
+
+        public float Health
+        {
+            get { return _health; }
+            set
+            {
+                _health = value;
+                if (_health <= 0)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+
+        public override string GetDisplayName()
+        {
+            return "Breakable Tile";
+        }
+
+
+        public void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                Health -= 30f;
+            }
+        }
+    }
+}
