281ba6c8cf70009953868862804ab0892f85757a
taesiri <taesiri@live.com>
2014-07-17 12:28:29 +0430

    Touch Buttons for MagicBall

--- ArenaBuilder/Assets/Scripts/Player/MagicBall.cs
+++ ArenaBuilder/Assets/Scripts/Player/MagicBall.cs
@@ -1,15 +1,17 @@
-﻿using UnityEngine;
+﻿using Assets.Scripts.Arena;
+using Assets.Scripts.Helpers;
+using UnityEngine;
 
 namespace Assets.Scripts.Player
 {
     public class MagicBall : MonoBehaviour
     {
+        private readonly GUILocationHelper _locationHelper = new GUILocationHelper();
         public float DampingX = 2.0f;
         public float DampingY = 2.0f;
         public float JumpHeight = 10.0f;
         public Transform PlayerCamera;
         public float Speed = 10.0f;
-
         private float _currentX;
         private float _currentY;
         private Transform _localTransform;
@@ -19,6 +21,8 @@ namespace Assets.Scripts.Player
         public void Start()
         {
             _localTransform = transform;
+            _locationHelper.PointLocation = GUILocationHelper.Point.BottomRight;
+            _locationHelper.UpdateLocation();
         }
 
         public void Update()
@@ -48,6 +52,27 @@ namespace Assets.Scripts.Player
             pos.x = _currentX;
 
             PlayerCamera.position = pos;
+        }
+
+        public void OnGUI()
+        {
+            GUI.matrix = AdvanceBrain.GUIMatrix;
+
+            if (GUI.RepeatButton(new Rect(_locationHelper.Offset.x - 200f, _locationHelper.Offset.y - 200f, 150, 150), ">"))
+            {
+                rigidbody.AddForce(Vector3.right*20*Speed*Time.deltaTime, ForceMode.Force);
+            }
+
+            if (GUI.RepeatButton(new Rect(_locationHelper.Offset.x - 400f, _locationHelper.Offset.y - 200f, 150, 150), "<"))
+            {
+                rigidbody.AddForce(Vector3.left*20*Speed*Time.deltaTime, ForceMode.Force);
+            }
+
+            if (GUI.Button(new Rect(10, _locationHelper.Offset.y - 200f, 150, 150), "Jump"))
+            {
+                rigidbody.AddForce(Vector3.up * JumpHeight, ForceMode.Impulse);
+            }
+            GUI.matrix = Matrix4x4.identity;
         }
     }
 }
